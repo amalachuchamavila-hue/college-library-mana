0893b2e6214c313f4051c0a735af3ec1f76b5aef
Amal.M <amalachuchamavila@gmail.com>
2026-09-17 07:59:52 +0530

    Implement global error handler in main.tsx
    Add global error handling to display errors in the UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+window.onerror = function(msg, url, line) {
+  document.body.innerHTML = '<pre style="color:red;padding:20px;font-size:16px;">Error: ' + msg + '\nAt: ' + line + '</pre>';
+};
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
